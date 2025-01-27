fb3f685b2306e45c2628e346e5a63b859466dc40
Mary Letey <mletey@boslogin07.rc.fas.harvard.edu>
2025-01-27 11:42:23 -0500

    test

--- Linear/Fig3_Mem_ICL_Transition_Task_Diversity/finitebayes.sh
+++ Linear/Fig3_Mem_ICL_Transition_Task_Diversity/finitebayes.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# largekappa_alpha1_d50.sbatch
+# longerrun_d80_alpha10.sbatch
 # 
-#SBATCH --job-name=largekappa_alpha1_d50
+#SBATCH --job-name=longerrun_d80_alpha10
 #SBATCH -c 1
 #SBATCH -t 1-00:00:00
 #SBATCH -p seas_compute
 #SBATCH --mem=48000
-#SBATCH -o /n/holyscratch01/pehlevan_lab/Lab/mletey/incontext-asymptotics-experiments/Linear/Fig3_Mem_ICL_Transition_Task_Diversity/pnas/outputfiledump/largekappa_alpha1_d50_%a.out
-#SBATCH -e /n/holyscratch01/pehlevan_lab/Lab/mletey/incontext-asymptotics-experiments/Linear/Fig3_Mem_ICL_Transition_Task_Diversity/pnas/outputfiledump/largekappa_alpha1_d50_%a.err
-#SBATCH --array=1-9
+#SBATCH -o /n/netscratch/pehlevan_lab/Lab/ml/incontext-asymptotics-experiments/Linear/Fig3_Mem_ICL_Transition_Task_Diversity/appendix/outputfiledump/longerrun_d80_alpha10_%a.out
+#SBATCH -e /n/netscratch/pehlevan_lab/Lab/ml/incontext-asymptotics-experiments/Linear/Fig3_Mem_ICL_Transition_Task_Diversity/appendix/outputfiledump/longerrun_d80_alpha10_%a.err
+#SBATCH --array=1-50
 #SBATCH --mail-type=END
 #SBATCH --mail-user=maryletey@fas.harvard.edu
 
 source activate try4
 
-parentdir="pnas"
+parentdir="appendix"
 newdir="$parentdir/job_${SLURM_JOB_NAME}"
 mkdir "$newdir"
-python finitebayesrun.py 50 $newdir 1 $SLURM_ARRAY_TASK_ID 0.1
+python finitebayesrun.py 80 $newdir 10 $SLURM_ARRAY_TASK_ID 0.1
--- Linear/Fig3_Mem_ICL_Transition_Task_Diversity/finitebayesrun.py
+++ Linear/Fig3_Mem_ICL_Transition_Task_Diversity/finitebayesrun.py
@@ -16,7 +16,7 @@ sigma_beta = 1
 # K = K_array[kappaind]
 N = np.int64(alpha * d)
 
-kappa_array = np.logspace(np.log10(0.01),np.log10(1000000),9)
+kappa_array = np.logspace(np.log10(500), np.log10(5000000), 50)
 K = int(kappa_array[kappaind]*d)
 
 nsim = 1000
@@ -28,44 +28,48 @@ e_B_finite_ary = np.zeros(nsim)
 print(f'kappa is {K/d}')
 B = np.random.randn(d, K)
 
-IsFinite = True
-for i in tqdm(range(nsim)):
-    X = np.random.randn(d, N) / np.sqrt(d)
-    if IsFinite:
-        beta = B[:, np.random.randint(K)].reshape(d, 1)
-    else:
-        beta = np.random.randn(d, 1) * sigma_beta
+# --------------------- IDG ERROR -----------------------
+# -------------------------------------------------------
+# IsFinite = True
+# for i in tqdm(range(nsim)):
+#     X = np.random.randn(d, N) / np.sqrt(d)
+#     if IsFinite:
+#         beta = B[:, np.random.randint(K)].reshape(d, 1)
+#     else:
+#         beta = np.random.randn(d, 1) * sigma_beta
 
-    y = X.T @ beta + np.random.randn(N, 1) * sigma_noise
+#     y = X.T @ beta + np.random.randn(N, 1) * sigma_noise
 
-    # # Bayesian estimator for the Gaussian distribution
-    # beta_hat = np.linalg.solve(X @ X.T + sigma_noise**2/sigma_beta**2 * np.eye(d), X @ y)
+#     # # Bayesian estimator for the Gaussian distribution
+#     # beta_hat = np.linalg.solve(X @ X.T + sigma_noise**2/sigma_beta**2 * np.eye(d), X @ y)
 
-    # Bayesian estimator for the finite distribution
-    c = -np.linalg.norm(y - X.T @ B, axis=0)**2/(2*sigma_noise**2)
-    ec = np.exp(c - np.max(c))
-    beta_hat_finite = B @ ec.reshape(K, 1) / np.sum(ec)
+#     # Bayesian estimator for the finite distribution
+#     c = -np.linalg.norm(y - X.T @ B, axis=0)**2/(2*sigma_noise**2)
+#     ec = np.exp(c - np.max(c))
+#     beta_hat_finite = B @ ec.reshape(K, 1) / np.sum(ec)
 
-    xv = np.random.randn(d, 1)/np.sqrt(d)
-    yv = (xv.T @ beta).item() + np.random.randn() * sigma_noise
+#     xv = np.random.randn(d, 1)/np.sqrt(d)
+#     yv = (xv.T @ beta).item() + np.random.randn() * sigma_noise
 
-    # e_B_full_ary[i] = ((xv.T @ beta_hat).item() - yv)**2
-    e_B_finite_ary[i] = ((xv.T @ beta_hat_finite).item() - yv)**2
+#     # e_B_full_ary[i] = ((xv.T @ beta_hat).item() - yv)**2
+#     e_B_finite_ary[i] = ((xv.T @ beta_hat_finite).item() - yv)**2
 
-ind = kappaind
-filename = f'{directory}/idg_dmmse_m.txt'
-with open(filename, 'a') as file:
-    file.write(f'{ind}, {np.mean(e_B_finite_ary)}\n')
-# filename = f'{directory}/idg_ridge_m.txt'
+# ind = kappaind
+# filename = f'{directory}/idg_dmmse_m.txt'
 # with open(filename, 'a') as file:
-#     file.write(f'{ind}, {np.mean(e_B_full_ary)}\n')
-filename = f'{directory}/idg_dmmse_s.txt'
-with open(filename, 'a') as file:
-    file.write(f'{ind}, {np.std(e_B_finite_ary)}\n')
-# filename = f'{directory}/idg_ridge_s.txt'
+#     file.write(f'{ind}, {np.mean(e_B_finite_ary)}\n')
+# # filename = f'{directory}/idg_ridge_m.txt'
+# # with open(filename, 'a') as file:
+# #     file.write(f'{ind}, {np.mean(e_B_full_ary)}\n')
+# filename = f'{directory}/idg_dmmse_s.txt'
 # with open(filename, 'a') as file:
-#     file.write(f'{ind}, {np.std(e_B_full_ary)}\n')
+#     file.write(f'{ind}, {np.std(e_B_finite_ary)}\n')
+# # filename = f'{directory}/idg_ridge_s.txt'
+# # with open(filename, 'a') as file:
+# #     file.write(f'{ind}, {np.std(e_B_full_ary)}\n')
 
+# --------------------- ICL ERROR -----------------------
+# -------------------------------------------------------
 IsFinite = False
 e_B_finite_ary = np.zeros(nsim)
 for i in range(nsim):
@@ -94,13 +98,13 @@ for i in range(nsim):
 ind = kappaind
 filename = f'{directory}/icl_dmmse_m.txt'
 with open(filename, 'a') as file:
-    file.write(f'[{ind}, {np.mean(e_B_finite_ary)}],')
+    file.write(f'[{ind+50}, {np.mean(e_B_finite_ary)}],')
 # filename = f'{directory}/icl_ridge_m.txt'
 # with open(filename, 'a') as file:
 #     file.write(f'[{ind}, {np.mean(e_B_full_ary)}],')
-filename = f'{directory}/icl_dmmse_s.txt'
-with open(filename, 'a') as file:
-    file.write(f'[{ind}, {np.std(e_B_finite_ary)}],')
+# filename = f'{directory}/icl_dmmse_s.txt'
+# with open(filename, 'a') as file:
+#     file.write(f'[{ind+50}, {np.std(e_B_finite_ary)}],')
 # filename = f'{directory}/icl_ridge_s.txt'
 # with open(filename, 'a') as file:
 #     file.write(f'[{ind}, {np.std(e_B_full_ary)}],')
